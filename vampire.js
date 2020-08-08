02e19e6c168472f8fe4bd523f931e31fa035215f
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-08 01:17:51 +0000

    this is the commit for both file

--- vampire.js
+++ vampire.js
@@ -10,22 +10,37 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator =this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numOfVam  =0;
+    let currentVampire =this;
+    while(currentVampire.creator){
+      currentVampire = currentVampire.creator;
+      numOfVam ++;
+    }
+    return numOfVam;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if(this.creator === null){
+      return true;
+   }else if(vampire.creator === null){
+      return false;
+   }else if (this.offspring.includes(vampire)){
+      return true;
+   }else if (!this.offspring.includes(vampire)){
+      return false;
+   }
   }
 
   /** Stretch **/
